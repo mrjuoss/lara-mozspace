d32eea4932344d8ae111ac9574d693db3bbaef55
mrjuoss <mrjuoss@gmail.com>
2017-08-19 16:08:56 +0700

    Routing dasar di laravel

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,15 @@ Route::get('/', function () {
 Route::get('/belajar', function () {
     return view('belajar');
 });
+
+Route::get('/test-parameter/{data}', function($data){
+  echo "Data yang diparsing adalah ".$data;
+});
+
+Route::get('/test-parameter-opsional/{data?}', function($data = 'tidak ada'){
+  echo "Data yang diparsing adalah ".$data;
+});
+
+Route::get('/test-dua-parameter/{data1}/{data2}', function($data1, $data2){
+  echo "Data yang diparsing adalah ".$data1." dan ".$data2;
+});
